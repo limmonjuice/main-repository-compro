2d1cdf65854ea077c7cae4215b3f3e182f6c00ec
limmonjuice <zaimond.lim@lorma.edu>
2025-02-13 09:38:07 +0800

    Final Ver

--- src/main/java/com/limmon/Main.java
+++ src/main/java/com/limmon/Main.java
@@ -9,17 +9,15 @@ public class Main {
         coffee1.setType("Arabica");
         coffee1.setSize("Medium");
         coffee1.setPrice(100.0);
+        coffee1.discount(10);
         coffee1.setRoastLevel("Dark");
         coffee1.setOrigin("Brazil");
         coffee1.setDecaf(false);
         coffee1.setStock(10);
+        coffee1.updateStock(3);
         coffee1.setBrewMethod("French Press");
         coffee1.setFlavorNotes(new String[]{"Chocolate", "Nutty"});
         coffee1.addFlavor("Matcha");
-
-        coffee1.discount(10);
-        coffee1.updateStock(3);
-
 
         System.out.println(coffee1.describe());
         System.out.println("---------------------------------\n");
@@ -29,18 +27,15 @@ public class Main {
         coffee2.setType("Robusta");
         coffee2.setSize("Large");
         coffee2.setPrice(150.0);
+        coffee2.discount(10);
         coffee2.setRoastLevel("Medium");
         coffee2.setOrigin("Colombia");
         coffee2.setDecaf(true);
         coffee2.setStock(5);
+        coffee2.updateStock(4);
         coffee2.setBrewMethod("Drip");
         coffee2.setFlavorNotes(new String[]{"Caramel", "Vanilla"});
         coffee2.addFlavor("Cinnamon");
-
-
-        coffee2.discount(10);
-        coffee2.updateStock(4);
-
 
         System.out.println(coffee2.describe());
         System.out.println("---------------------------------");
