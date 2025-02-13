823d290bb500c839d9d6c80b3b26056478c53d68
limmonjuice <zaimond.lim@lorma.edu>
2025-02-14 02:22:13 +0800

    Final Commit

--- src/main/java/com/limmon/Coffee.java
+++ src/main/java/com/limmon/Coffee.java
@@ -1,110 +1,130 @@
 package com.limmon;
 
-class Coffee {
-    private String name;
-    private String type;
-    private String size;
-    private double price;
-    private double originalPrice;
-    private String roastLevel;
-    private String origin;
-    private boolean isDecaf;
-    private int stock;
-    private int originalStock;
-    private String[] flavorNotes;
-    private String brewMethod;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    public Coffee() {
-        this.name = "";
-        this.type = "";
-        this.size = "";
-        this.price = 0.0;
-        this.originalPrice = 0.0;
-        this.roastLevel = "";
-        this.origin = "";
-        this.isDecaf = false;
-        this.stock = 0;
-        this.originalStock = 0;
-        this.brewMethod = "";
-        this.flavorNotes = new String[0];
-    }
+public class Coffee {
 
-    public void setName(String name) {
+    //attributes of Coffee
+    String name;
+    String type;
+    String size;
+    double price;
+    String roastLevel;
+    String origin;
+    boolean isDecaf;
+    int stock;
+    public ArrayList<String> flavorNotes;
+    String brewMethod;
+
+    /**
+     * constructor
+     *
+     * @param name         The name of the coffee.
+     * @param type         The type of coffee (e.g., espresso, latte).
+     * @param size         The size of the coffee (small, medium, large).
+     * @param price        The base price of the coffee.
+     * @param roastLevel   The roast level of the coffee (e.g., light, medium, dark).
+     * @param origin       The origin of the coffee beans.
+     * @param isDecaf      Flag indicating whether the coffee is decaffeinated.
+     * @param stock        The current stock of the coffee.
+     * @param flavorNotes  Array of flavor notes for the coffee.
+     * @param brewMethod   The method used to brew the coffee (e.g., drip, French press).
+     */
+    public Coffee(String name, String type, String size, double price, String roastLevel, String origin,
+                   boolean isDecaf, int stock, String[] flavorNotes, String brewMethod) {
         this.name = name;
-    }
-    public void setType(String type) {
         this.type = type;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-        this.originalPrice = price;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
-        if (this.originalStock == 0) {
-            this.originalStock = stock;
-        }
-    }
-    public void discount(double percentage) {
-        this.price = this.originalPrice - (this.originalPrice * (percentage / 100));
-    }
-    public void setSize(String size) {
         this.size = size;
-    }
-    public void setRoastLevel(String roastLevel) {
+        this.price = 100; //Base price for all coffees
         this.roastLevel = roastLevel;
-    }
-    public void setOrigin(String origin) {
         this.origin = origin;
-    }
-    public void setDecaf(boolean isDecaf) {
         this.isDecaf = isDecaf;
-    }
-    public void setBrewMethod(String brewMethod) {
+        this.stock = stock;
+        this.flavorNotes = new ArrayList<>(Arrays.asList(flavorNotes));
         this.brewMethod = brewMethod;
     }
-    public void setFlavorNotes(String[] flavorNotes) {
-        this.flavorNotes = flavorNotes;
-    }
-    public void addFlavor(String note) {
-        String[] newFlavorNotes = new String[this.flavorNotes.length + 1];
-        for (int i = 0; i < this.flavorNotes.length; i++) {
-            newFlavorNotes[i] = this.flavorNotes[i];
+
+    /**
+     * method to calculate the price based on the size of the coffee.
+     *
+     * @param size The size of the coffee (small, medium, large).
+     */
+    public void calculatePrice(String size) {
+        switch (size.toLowerCase()) {
+            case "small":
+                this.price += 10;
+                break;
+            case "medium":
+                this.price += 30;
+                break;
+            case "large":
+                this.price += 50;
+                break;
+            default:
+                System.out.println("Invalid size. Keeping base price.");
         }
-        newFlavorNotes[this.flavorNotes.length] = note;
-        this.flavorNotes = newFlavorNotes;
+        System.out.println("Price of " + this.name + " (" + this.size + "): PHP " + this.price);
     }
+
+    //method to check if the coffee is in stock.
+    public void checkStock() {
+        System.out.println("Stock for " + this.name + ": " + (this.stock > 0 ? "Available" : "Out of stock"));
+    }
+
+    /**
+     * method to add a new flavor note to the coffee.
+     *
+     * @param note The flavor note to add.
+     */
+    public void addFlavor(String note) {
+        this.flavorNotes.add(note);
+        System.out.println("Added flavor: " + note);
+    }
+
+    /**
+     * method to update the stock of the coffee.
+     *
+     * @param quantity The quantity to add to the stock.
+     */
     public void updateStock(int quantity) {
         this.stock += quantity;
+        System.out.println("Updated stock for " + this.name + ": " + this.stock);
     }
 
-    public String describe() {
-        StringBuilder description = new StringBuilder();
-        description.append("Coffee Name: ").append(this.name).append("\n");
-        description.append("Type: ").append(this.type).append("\n");
-        description.append("Size: ").append(this.size).append("\n");
-        description.append("Original Price: PHP ").append(this.originalPrice).append("\n");
-        description.append("Discounted Price: PHP ").append(this.price).append("\n");
-        description.append("Roast Level: ").append(this.roastLevel).append("\n");
-        description.append("Origin: ").append(this.origin).append("\n");
-        description.append("Decaf: ").append(this.isDecaf ? "Yes" : "No").append("\n");
-        description.append("Original Stock: ").append(this.originalStock).append("\n");
-        description.append("Updated Stock: ").append(this.stock).append("\n");
-        description.append("Brew Method: ").append(this.brewMethod).append("\n");
-        description.append("Flavor Notes: ");
-        if (this.flavorNotes.length == 0) {
-            description.append("No flavor notes");
-        } else {
-            for (int i = 0; i < this.flavorNotes.length; i++) {
-                description.append(this.flavorNotes[i]);
-                if (i < this.flavorNotes.length - 1) {
-                    description.append(", ");
-                }
-            }
-        }
-        description.append("\n");
-        return description.toString();
+    //method to describe the coffee including its roast level and flavor notes.
+    public void describe() {
+        System.out.println("Description: A " + this.roastLevel + " roast " + this.name + " with " + String.join(", ", this.flavorNotes) + " notes.");
     }
 
+    /**
+     * method to update the decaf status of the coffee.
+     *
+     * @param isDecaf Flag indicating whether the coffee is decaffeinated.
+     */
+    public void setDecaf(boolean isDecaf) {
+        this.isDecaf = isDecaf;
+        System.out.println(this.name + " is now " + (this.isDecaf ? "decaffeinated" : "regular"));
+    }
 
+    /**
+     * method to change the roast level of the coffee.
+     *
+     * @param newRoastLevel The new roast level for the coffee.
+     */
+    public void changeRoastLevel(String newRoastLevel) {
+        this.roastLevel = newRoastLevel;
+        System.out.println("Roast level of " + this.name + " changed to " + this.roastLevel);
+    }
+
+    /**
+     * method to apply a discount to the price of the coffee.
+     *
+     * @param percentage The discount percentage to apply.
+     */
+    public void discount(int percentage) {
+        double discountAmount = (this.price * percentage) / 100;
+        this.price -= discountAmount;
+        System.out.println("Discount applied! New price of " + this.name + ": PHP " + this.price);
+    }
 }
--- src/main/java/com/limmon/Main.java
+++ src/main/java/com/limmon/Main.java
@@ -2,42 +2,98 @@ package com.limmon;
 
 public class Main {
     public static void main(String[] args) {
+        //first coffee object
+        Coffee coffee1 = new Coffee(
+                "Espresso", "Arabica", "Medium", 100, "Dark", "Colombia", false, 10,
+                new String[]{"Chocolate", "Nutty"}, "French Press"
+        );
 
-        Coffee coffee1 = new Coffee();
+        //second coffee object
+        Coffee coffee2 = new Coffee(
+                "Latte", "Robusta", "Large", 100, "Medium", "Brazil", false, 5,
+                new String[]{"Caramel", "Creamy"}, "Drip"
+        );
 
-        coffee1.setName("Espresso");
-        coffee1.setType("Arabica");
-        coffee1.setSize("Medium");
-        coffee1.setPrice(100.0);
-        coffee1.discount(10);
-        coffee1.setRoastLevel("Dark");
-        coffee1.setOrigin("Brazil");
-        coffee1.setDecaf(false);
-        coffee1.setStock(10);
-        coffee1.updateStock(3);
-        coffee1.setBrewMethod("French Press");
-        coffee1.setFlavorNotes(new String[]{"Chocolate", "Nutty"});
-        coffee1.addFlavor("Matcha");
+        //properties for coffee 1
+        System.out.println("Coffee 1 Details:");
+        System.out.println("Name: " + coffee1.name);
+        System.out.println("Type: " + coffee1.type);
+        System.out.println("Size: " + coffee1.size);
+        System.out.println("Base Price of all coffees: PHP" + coffee1.price);
+        System.out.println("Roast Level: " + coffee1.roastLevel);
+        System.out.println("Origin: " + coffee1.origin);
+        System.out.println("Is Decaf: " + coffee1.isDecaf);
+        System.out.println("Stock: " + coffee1.stock);
+        System.out.println("Flavor Notes: " + String.join(", ", coffee1.flavorNotes));
+        System.out.println("Brew Method: " + coffee1.brewMethod);
+        System.out.println();
 
-        System.out.println(coffee1.describe());
-        System.out.println("---------------------------------\n");
+        //properties for coffee 2
+        System.out.println("Coffee 2 Details:");
+        System.out.println("Name: " + coffee2.name);
+        System.out.println("Type: " + coffee2.type);
+        System.out.println("Size: " + coffee2.size);
+        System.out.println("Base Price of all coffees: PHP" + coffee2.price);
+        System.out.println("Roast Level: " + coffee2.roastLevel);
+        System.out.println("Origin: " + coffee2.origin);
+        System.out.println("Is Decaf: " + coffee2.isDecaf);
+        System.out.println("Stock: " + coffee2.stock);
+        System.out.println("Flavor Notes: " + String.join(", ", coffee2.flavorNotes));
+        System.out.println("Brew Method: " + coffee2.brewMethod);
+        System.out.println();
 
-        Coffee coffee2 = new Coffee();
-        coffee2.setName("Latte");
-        coffee2.setType("Robusta");
-        coffee2.setSize("Large");
-        coffee2.setPrice(150.0);
+        //methods for coffee 1
+        System.out.println("\n=== Coffee 1 ===");
+        coffee1.describe();
+        coffee1.calculatePrice(coffee1.size);
+        coffee1.updateStock(5);
+        coffee1.checkStock();
+        coffee1.addFlavor("Vanilla");
+        coffee1.setDecaf(true);
+        coffee1.changeRoastLevel("Light");
+        coffee1.discount(15);
+
+        //methods for coffee 2
+        System.out.println("\n=== Coffee 2 ===");
+        coffee2.describe();
+        coffee2.calculatePrice(coffee2.size);
+        coffee2.updateStock(-2);
+        coffee2.checkStock();
+        coffee2.addFlavor("Hazelnut");
+        coffee2.setDecaf(false);
+        coffee2.changeRoastLevel("Dark");
         coffee2.discount(10);
-        coffee2.setRoastLevel("Medium");
-        coffee2.setOrigin("Colombia");
-        coffee2.setDecaf(true);
-        coffee2.setStock(5);
-        coffee2.updateStock(4);
-        coffee2.setBrewMethod("Drip");
-        coffee2.setFlavorNotes(new String[]{"Caramel", "Vanilla"});
-        coffee2.addFlavor("Cinnamon");
 
-        System.out.println(coffee2.describe());
-        System.out.println("---------------------------------");
+        //updated coffee 1 details
+        System.out.println();
+        System.out.println("Updated Coffee 1 Details:");
+        coffee1.describe();
+        System.out.println("Name: " + coffee1.name);
+        System.out.println("Type: " + coffee1.type);
+        System.out.println("Size: " + coffee1.size);
+        System.out.println("Updated Price: PHP" + coffee1.price);
+        System.out.println("Roast Level: " + coffee1.roastLevel);
+        System.out.println("Origin: " + coffee1.origin);
+        System.out.println("Is Decaf: " + coffee1.isDecaf);
+        System.out.println("Stock: " + coffee1.stock);
+        System.out.println("Flavor Notes: " + String.join(", ", coffee1.flavorNotes));
+        System.out.println("Brew Method: " + coffee1.brewMethod);
+        System.out.println();
+
+        //updated coffee 2 details
+        System.out.println();
+        System.out.println("Updated Coffee 2 Details:");
+        coffee1.describe();
+        System.out.println("Name: " + coffee2.name);
+        System.out.println("Type: " + coffee2.type);
+        System.out.println("Size: " + coffee2.size);
+        System.out.println("Updated Price: PHP" + coffee2.price);
+        System.out.println("Roast Level: " + coffee2.roastLevel);
+        System.out.println("Origin: " + coffee2.origin);
+        System.out.println("Is Decaf: " + coffee2.isDecaf);
+        System.out.println("Stock: " + coffee2.stock);
+        System.out.println("Flavor Notes: " + String.join(", ", coffee2.flavorNotes));
+        System.out.println("Brew Method: " + coffee2.brewMethod);
+        System.out.println();
     }
 }
