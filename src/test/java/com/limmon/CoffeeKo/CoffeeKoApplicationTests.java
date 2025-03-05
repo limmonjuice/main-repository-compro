4c4754fdb67b17dcb59b431c9b6626734f2c5f9a
limmonjuice <zaimond.lim@lorma.edu>
2025-03-05 08:56:19 +0800

    Init

---
+++ src/test/java/com/limmon/CoffeeKo/CoffeeKoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limmon.CoffeeKo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeKoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
