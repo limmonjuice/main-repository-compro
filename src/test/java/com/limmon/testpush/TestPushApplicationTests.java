bf5d241d28ec9d11e1b0e2bc7fa74b2a41840e8f
limmonjuice <zaimond.lim@lorma.edu>
2025-05-23 09:46:33 +0800

    try

---
+++ src/test/java/com/limmon/testpush/TestPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limmon.testpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
